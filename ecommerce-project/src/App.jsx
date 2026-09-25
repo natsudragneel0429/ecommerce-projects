8ae8992ad3b8a5f8a4cf8b68956784258c268208
Drew Romano <kristianandrew29@gmail.com>
2026-09-25 09:38:25 +0800

    Create add t cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -9,17 +9,17 @@ import axios from 'axios'
 
 function App() {
       const [cart,setCart]= useState([]);
-
+       const loadCart = async()=>{
+        const response= await axios.get('/api/cart-items?expand=product');
+                      setCart(response.data);
+            };
       useEffect(()=>{
             // axios.get('/api/cart-items?expand=product')
             //       .then((response)=>{
             //           setCart(response.data);
             //       });
-            const fetchAppData = async()=>{
-        const response= await axios.get('/api/cart-items?expand=product');
-                      setCart(response.data);
-            };
-    fetchAppData();
+     
+    loadCart();
                  
       },[]);
             
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart}/>} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart}/>} />
       <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<TrackingPage />} />
--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -3,7 +3,7 @@ import { useEffect,useState } from 'react';
 import { Header } from '../../components/header';
 import './HomePage.css';
 import { ProductsGrid } from './ProductsGrid';
-export function HomePage({cart}) {
+export function HomePage({cart, loadCart}) {
 
     const [products,setProducts]= useState([]);
 
@@ -25,7 +25,7 @@ export function HomePage({cart}) {
             <Header cart={cart} />
 
             <div className="home-page">
-            <ProductsGrid products={products} />
+            <ProductsGrid products={products} loadCart={loadCart} />
             </div>
         </>
     );
