54e6fd193b5c87315aab60ea7a6066554916e827
Drew Romano <kristianandrew29@gmail.com>
2026-09-25 14:12:15 +0800

    changes Header in Orderpage

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState, Fragment } from 'react';
-import { Header } from '../../components/header';
+import { Header } from '../../components/Header';
 import './OrdersPage.css';
 import dayjs from 'dayjs';
 import { formatMoney } from '../../utils/money';
