5913ebf8565a3a6880d44cbc35c4c817292e81e4
Drew Romano <kristianandrew29@gmail.com>
2026-09-25 14:05:28 +0800

    cahnge homepage

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect,useState } from 'react';
-import { Header } from '../../components/header';
+import {Header}from '../../components/Header';
 import './HomePage.css';
 import { ProductsGrid } from './ProductsGrid';
 export function HomePage({cart, loadCart}) {
