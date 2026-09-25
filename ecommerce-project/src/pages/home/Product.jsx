db1d550eee05d1214be46a9e5d2507b74b14d7d2
Drew Romano <kristianandrew29@gmail.com>
2026-09-25 12:13:57 +0800

    Create integration test for home page

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -17,7 +17,8 @@ export function Product({product, loadCart}){
                                             };
     return(
         <>
-                                            <div className="product-container">
+                                            <div className="product-container"
+                                            data-testID="product-container">
                                         <div className="product-image-container">
                                             <img className="product-image"
                                             data-testid="product-image"
