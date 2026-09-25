b3ac4d07301c66cfee6c0eb1c1a31d4461901e7f
Drew Romano <kristianandrew29@gmail.com>
2026-09-25 08:48:53 +0800

    Seperate into smaller components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { HomePage } from './pages/Homepage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { HomePage } from './pages/home/HomePage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
 import { Routes, Route } from 'react-router'
